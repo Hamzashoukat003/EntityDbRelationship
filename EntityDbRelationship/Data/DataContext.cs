d7138fcbaa9b82a0b0233f198edd7fe6da1d503d
HAMZA SHOUKAT <hamzashoukat32@outlook.com>
2024-03-05 13:31:31 +0500

    Add project files.

---
+++ EntityDbRelationship/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using EntityDbRelationship.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityDbRelationship.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options
+            ) :base(options) { }
+        public DbSet<Character> CharacterSet { get; set; }
+        public DbSet<Backpack> BackpackSet { get; set; }
+
+    }
+}
---
+++ EntityDbRelationship/Models/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityDbRelationship.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BackpackId { get; set; }
+        public Backpack Backpack { get; set; }
+    }
+}
